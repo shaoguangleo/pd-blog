54b3d9aff350ef0d3001c11da7c7373edba6215f
Guo Shaoguang <sgguo@shao.ac.cn>
2017-12-14 09:57:33 +0800

    Adding another model - Product

--- mblog/mainsite/admin.py
+++ mblog/mainsite/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from .models import Post
+from .models import Product
 
 # Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'pub_date')
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('sku', 'name', 'price', 'size')
+
 admin.site.register(Post, PostAdmin)
+admin.site.register(Product, ProductAdmin)
--- mblog/mainsite/models.py
+++ mblog/mainsite/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
 
 class Post(models.Model):
@@ -14,3 +15,15 @@ class Post(models.Model):
 
     def __unicode__(self):
         return self.title
+
+
+class Product(models.Model):
+    SIZES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
+    sku = models.CharField(max_length=5)
+    name = models.CharField(max_length=20)
+    price = models.PositiveIntegerField()
+    size = models.CharField(max_length=1, choices=SIZES)
